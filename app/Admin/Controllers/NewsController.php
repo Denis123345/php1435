266069eae2957c4db58892878fc2c2e046324b6d
Зиновьев1 Денис <zinovev1dv@111c11.occ.local>
2023-11-25 09:20:50 +0400

    laravel admin

---
+++ app/Admin/Controllers/NewsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\News;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class NewsController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'News';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new News());
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('Title'));
+        $grid->column('preview', __('Preview'));
+        $grid->column('content', __('Content'));
+        $grid->column('is_active', __('Is active'));
+        $grid->column('published_at', __('Published at'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+        $grid->column('deleted_at', __('Deleted at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(News::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('title', __('Title'));
+        $show->field('preview', __('Preview'));
+        $show->field('content', __('Content'));
+        $show->field('is_active', __('Is active'));
+        $show->field('published_at', __('Published at'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+        $show->field('deleted_at', __('Deleted at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new News());
+
+        $form->text('title', __('Title'));
+        $form->textarea('preview', __('Preview'));
+        $form->textarea('content', __('Content'));
+        $form->switch('is_active', __('Is active'));
+        $form->datetime('published_at', __('Published at'))->default(date('Y-m-d H:i:s'));
+
+        return $form;
+    }
+}
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -6,6 +6,7 @@
       <tr>
         <td>{{ $item->id }}</td>
         <td>{{ $item->title }}</td>
+        <td><a href="{{ route('news.show',$item->id) }}">{{$item->title }}<a></td> 
         <td>{{ $item->created_at }}</td>
         <td>{{ $item->preview }}</td>
       </tr>
