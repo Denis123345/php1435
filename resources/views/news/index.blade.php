29c93f20f2630f794674e1021a2998ababb455a7
Зиновьев1 Денис <zinovev1dv@115c9.occ.local>
2023-12-02 11:53:05 +0400

    news samples

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<table>
-    @foreach($items as $item)
-      <tr>
-        <td>{{ $item->id }}</td>
-        <td>{{ $item->title }}</td>
-        <td><a href="{{ route('news.show',$item->id) }}">{{$item->title }}<a></td> 
-        <td>{{ $item->created_at }}</td>
-        <td>{{ $item->preview }}</td>
-      </tr>
-    @endforeach
-</table>
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-md-12">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">Navbar</a>
+        </nav>
+        <div class="card">
+          <div class="card-body">
+            <table class="table table-striped">
+              @foreach($items as $item)
+                <tr>
+                  <td>{{ $item->id }}</td>
+                  <td>{{ $item->title }}</td>
+                  <td><a href="{{ route('news.show',$item->id) }}">{{$item->title }}<a></td> 
+                  <td>{{ $item->created_at }}</td>
+                  <td>{{ $item->preview }}</td>
+                </tr>
+              @endforeach
+            </table>
+          </div>
+        </div> 
+      </div>
+    </div>
+  </div>
+                
+
 @endsection
