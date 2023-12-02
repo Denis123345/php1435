0a4c265299a58ecadcbda52cfed8b8947c0b68e6
Акжигитов Шамиль <akzhigitovsha@115c10.occ.local>
2023-12-02 11:52:27 +0400

    Стили шаблоны show

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-        <h1>{{ $item->title }}</h1>
-        <div><i>{{ $item->preview }}</i></div>
-        <div>{{ $item->content }}</div>
-        <p>{{ $item->is_active }}</p>
-        <p>{{ $item->published_at }}</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                       <div class="row"> 
+                          <div class="col-lg-4 col-md-4 col-sm-12">
+                             <p>{{ $item->published_at }}</p>  
+                           </div>
+                           <div class="col-lg-4 col-md-4 col-sm-12">
+                             <h1 class="mt-2"><span class="text-justify">Название: </span>{{ $item->title }}</h1>
+                             <div class="mt-1"><span class="text-justify">Аннотация: </span><i>{{ $item->preview }}</i></div>
+                             <div class="mt-1">{{ $item->content }}</div>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
